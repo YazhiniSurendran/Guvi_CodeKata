797c4ac793a9764d3330460d1dab259cde0bc794
Yazhini Surendran <30383540+YazhiniSurendran@users.noreply.github.com>
2018-07-12 02:32:09 -0700

    Update B_set_5.py

--- B_set_5.py
+++ B_set_5.py
@@ -1,4 +1,4 @@
-num1 = int(input())
+'''num1 = int(input())
 num2 = int(input())
 num3 = int(input())
 
@@ -9,4 +9,18 @@ if num1 > num2:
   elif num2 > num3:
   	print(num2)
 else:
-  print(num3) 
+  print(num3) '''
+
+a = input()
+a = a.split()
+num1 = a[0]
+num2 = a[1]
+num3 = a[2]
+
+if num1 > num2:
+  if num1 > num3:
+    print(num1)
+  elif num2 > num3:
+  	print(num2)
+else:
+  print(num3)
