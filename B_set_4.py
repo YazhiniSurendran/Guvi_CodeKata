387842296a2e93031febafe43cca597232780b88
Yazhini Surendran <30383540+YazhiniSurendran@users.noreply.github.com>
2018-07-12 01:53:57 -0700

    Create B_set_4.py

---
+++ B_set_4.py
@@ -0,0 +1,8 @@
+letter = input()
+alpha = ['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+
+letter = letter.lower()
+if letter in alpha:
+	print("yes")
+else:
+	print("No") 
